4ec6bf57598ea3e2def334c4dc1a6934edb3d83c
Carl Vänman <70966062+carl-vanman@users.noreply.github.com>
2022-12-18 19:58:54 +0100

    Helper functions, and outerFn with innerFn to determine packet status,return true/ false

---
+++ script.js
@@ -0,0 +1,131 @@
+function isInteger(input){
+    return typeof input === "number";
+}
+
+function isLeftHandSideLower(leftHandSide, rightHandSide){
+    return leftHandSide < rightHandSide;
+}
+
+function isLeftHandSideHigher(leftHandSide, rightHandSide){
+    return leftHandSide > rightHandSide;
+}
+
+function isArray(input) {
+    return Array.isArray(input);
+}
+
+function isUndefined(input) {
+    return input === undefined;
+}
+
+function isEqual(leftHandSide, rightHandSide){
+    return leftHandSide === rightHandSide;
+}
+
+
+function totalScore(scoreList) {
+    return scoreList.reduce( (acc, curr) =>  ( acc + curr ), 0);
+}
+
+let indexCounter = 0;
+const score = [];
+
+function outerFn(arr1, arr2){
+    let index = 0;
+
+    function innerFn(left, right){
+
+        //steg 1
+        if(( isUndefined(left) && isArray(right) ) || ( isUndefined(left) && isInteger(right) )){
+            return true;
+        }
+        
+        //steg 2
+        if(( isArray(left) && isUndefined(right) ) || ( isInteger(left) && isUndefined(right) ) || ( isUndefined(left) && isUndefined(right) )){
+            return false;
+        }
+
+        //steg 3
+        if( isInteger(left) && isInteger(right) ){
+
+            //steg 3.1
+            if( left < right ){
+                return true;
+            }
+
+            //steg 3.2
+            if( left > right ){
+                return false;
+            }
+
+            //steg 3.3
+            if( left === right ){
+                index++;
+                return innerFn( arr1[index], arr2[index] )
+            }
+        }
+
+        //steg 4
+        if( isArray(left) && isArray(right) ){
+            let result = outerFn( left, right );
+
+            if(result){
+                return result;
+            }
+            
+            index++;
+            return innerFn( arr1[index], arr2[index] )
+        }
+
+        //steg 5
+        if( isArray(left) && isInteger(right) ){
+            let result = outerFn( left, [ right ] );
+
+            if(result){
+                return result;
+            }
+            
+            index++;
+            return innerFn( arr1[index], arr2[index] )
+        }
+        
+        //steg 6
+        if( isInteger(left) && isArray(right) ){
+            let result = outerFn( [ left ], right );
+
+            if(result){
+                return result;
+            }
+            
+            index++;
+            return innerFn( arr1[index], arr2[index] )
+        }
+    }
+
+    return innerFn( arr1[index], arr2[index] );
+}
+
+// #1
+console.log( outerFn([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) ); //true
+
+// #2
+console.log( outerFn([[1],[2,3,4]], [[1],4]) ); //true
+
+// #3
+console.log( outerFn([9], [[8,7,6]]) ); //false
+
+// #4
+console.log( outerFn([[4,4],4,4], [[4,4],4,4,4]) ); //true
+
+// #5
+console.log( outerFn([7,7,7,7], [7,7,7]) ); //false
+
+// #6
+console.log( outerFn([], [3]) ); //true
+
+// #7
+console.log( outerFn([[[]]], [[]]) ); //false
+
+// #8
+console.log( outerFn([1,[2,[3,[4,[5,6,7]]]],8,9], [1,[2,[3,[4,[5,6,0]]]],8,9]) ); //false
+
